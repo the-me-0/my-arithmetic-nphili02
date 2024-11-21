8c60c129add22b85fe543dbcb0f07df3c9e6feaa
nphili02 <2361-nphili02@users.noreply.gitlab.univ-lr.fr>
2024-11-21 10:59:11 +0100

    docs: added sphinx doc

---
+++ docs/index.rst
@@ -0,0 +1,24 @@
+.. my-arithmetic-nphili02 documentation master file, created by
+   sphinx-quickstart on Thu Nov 21 10:23:42 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to my-arithmetic-nphili02's documentation!
+==================================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   functions
+   tests/test_functions
+
+API Documentation
+=================
+
+.. automodule:: my_arithmetic_nphili02.functions
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+
--- src/my_arithmetic_nphili02/functions.py
+++ src/my_arithmetic_nphili02/functions.py
@@ -1,8 +1,27 @@
 # pgcd_project/pgcd.py
 def pgcd(a: int, b: int) -> int:
     """
-    Calcul du plus grand commun diviseur (PGCD) de deux nombres.
-    Utilise l'algorithme d'Euclide.
+    Calcule le plus grand commun diviseur (PGCD) de deux nombres en utilisant l'algorithme d'Euclide.
+
+    Parameters
+    ----------
+    a : int
+        Le premier nombre.
+    b : int
+        Le deuxième nombre.
+
+    Returns
+    -------
+    int
+        Le PGCD des deux nombres.
+
+    Examples
+    --------
+    >>> pgcd(56, 98)
+    14
+
+    >>> pgcd(101, 10)
+    1
     """
     while b != 0:
         a, b = b, a % b
